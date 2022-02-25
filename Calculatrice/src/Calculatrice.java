111fcf8297bb44fba03aa55a1df81b728e2c2773
damien-mathieu1 <damien.trot34@gmail.com>
2022-02-25 09:01:00 +0100

    Début de développement de la calculatrice basique

---
+++ Calculatrice/src/Calculatrice.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Calculatrice {
+	public static void main(String[] args) 
+    {
+		/*Création de la fenetre de la calculatrice*/
+        JFrame fenetre = new JFrame("Calculatrice");
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenetre.setSize(300,400);
+        fenetre.setVisible(true);
+        /*Set up d'un pannel qui va contenir les boutons*/
+        
+    }
+}
